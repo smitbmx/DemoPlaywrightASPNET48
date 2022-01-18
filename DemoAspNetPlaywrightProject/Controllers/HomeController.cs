24c388e11ba403275eb3a3005e780f803ef003eb
Ivan Brygar <ivb@sitecore.net>
2022-01-18 17:28:32 +0800

    Added a simple screenshot generation implementation for onload page (Index action).

--- DemoAspNetPlaywrightProject/Controllers/HomeController.cs
+++ DemoAspNetPlaywrightProject/Controllers/HomeController.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Playwright;
 
 namespace DemoAspNetPlaywrightProject.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            using (var playwright = await Playwright.CreateAsync())
+            {
+                var browser = await playwright.Webkit.LaunchAsync();
+                var page = await browser.NewPageAsync();
+                await page.GotoAsync("https://github.com/microsoft/playwright");
+                await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+                await browser.DisposeAsync();
+            }
+
             return View();
         }
 
